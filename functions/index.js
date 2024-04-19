64bd43285dae7843dc565226707d2a85962aa069
Yung-Hsiang Chiu <mirror.tw@gmail.com>
2024-04-19 13:47:05 +0800

    feat: handle sticker and location

--- functions/index.js
+++ functions/index.js
@@ -29,6 +29,16 @@ const replyTextMessage = (event, text) =>
     messages: [{ type: "text", text }],
   });
 
+const onStickerMessage = async (event) => {
+  const { packageId, stickerId } = event.message;
+  return replyTextMessage(event, `這個貼圖的packageId: ${packageId} stickerId: ${stickerId}`);
+};
+
+const onLocationMessage = async (event) => {
+  const { title, address, latitude, longitude } = event.message;
+  return replyTextMessage(event, `這個地點是 ${title} 地址: ${address} 緯經度: (${latitude}, ${longitude})`);
+};
+
 const onTextMessage = async (event) => {
   const text = event.message.text || "<空訊息>";
   const uid = event.source.userId;
@@ -47,7 +57,7 @@ const onTextMessage = async (event) => {
     default:
       return replyTextMessage(event, text);
   }
-}
+};
 
 const handleEvent = async (event) => {
   if (event.type !== "message") {
@@ -55,7 +65,9 @@ const handleEvent = async (event) => {
   }
   const type = event.message.type;
   switch (type) {
-    case "text": return await onTextMessage(event);
+    case "text": return await onTextMessage(event); // 文字訊息
+    case "sticker": return await onStickerMessage(event); // 貼圖訊息
+    case "location": return await onLocationMessage(event); // 地點訊息
     default: return replyTextMessage(event, `尚未支援的訊息類型: ${type}`);
   }
 };
